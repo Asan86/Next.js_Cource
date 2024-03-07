08c5fcaa5bd30123a4b8a12d15317161be4926a7
Asan Turdukulov <asanturdukulov86@gmail.com>
2024-03-07 23:05:55 +0600

    Ссылка на навигацию по компонентам

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,14 @@
+import Link from "next/link";
 import React from "react";
 
 function HomePage() {
-  return <h1>HomePage</h1>;
+  return (
+    <>
+      <h1>Welcome Home!</h1>
+      <Link href="/blog">Blog</Link>
+      <Link href="/products">Products</Link>
+    </>
+  );
 }
 
 export default HomePage;
--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -1,12 +1,26 @@
+import Link from "next/link";
 import React from "react";
 
 export default function ProductList() {
+  const productId = 100;
   return (
     <>
+      <Link href="/">Home</Link>
       <h1>ProductList</h1>
-      <h2>Produkt 1</h2>
-      <h2>Produkt 2</h2>
-      <h2>Produkt 3</h2>
+      <h2>
+        <Link href="products/1">Produkt 1</Link>
+      </h2>
+      <h2>
+        <Link href="products/2">Produkt 2</Link>
+      </h2>
+      <h2>
+        <Link href="products/3" replace>
+          Produkt 3
+        </Link>
+      </h2>
+      <h2>
+        <Link href={`products/${productId}`}>Produkt {productId}</Link>
+      </h2>
     </>
   );
 }
